5525f149d0b068985db772ccda0103ecc1b03885
Kotaro Kuroda <jolly96k@gmail.com>
2024-07-13 18:14:09 +0900

    chore: 開発サーバーを起動するスクリプトを作成

---
+++ script/run_hugo_server.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+HUGO_PROJECT_PATH=$(cd $(dirname ${0}) && cd .. && pwd)
+
+cd ${HUGO_PROJECT_PATH}
+rm -rf .hugo_build.lock public resources
+hugo server -DEF --disableFastRender --gc
