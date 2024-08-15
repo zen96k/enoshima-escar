ba663c363bd6354a6789743ffc3984afff9687a9
zen96k <jolly96k@gmail.com>
2024-08-15 17:36:46 +0900

    refactor: 空行を追加

--- script/run_hugo_server.sh
+++ script/run_hugo_server.sh
@@ -4,5 +4,6 @@ set -euxo pipefail
 HUGO_PROJECT_PATH=$(cd $(dirname ${0}) && cd .. && pwd)
 
 cd ${HUGO_PROJECT_PATH}
+
 rm -rf .hugo_build.lock public resources
 hugo server -DEF --disableFastRender --gc
