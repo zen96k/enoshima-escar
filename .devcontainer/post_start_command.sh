04abade58f0b14ba1d0e4c4622d9f05d680519c9
zen96k <jolly96k@gmail.com>
2025-02-22 11:05:29 +0900

    chore: 開発環境を調整

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -2,9 +2,12 @@
 
 set -euxo pipefail
 
-rm -rf ${HOME}/.gitconfig
-git config --global init.defaultBranch ${GIT_INIT_DEFAULT_BRANCH}
-git config --global user.name ${GIT_USER_NAME}
-git config --global user.email ${GIT_USER_EMAIL}
+rm -rf "${HOME}"/.gitconfig
+git config --global init.defaultBranch "${GIT_INIT_DEFAULT_BRANCH}"
+git config --global user.name "${GIT_USER_NAME}"
+git config --global user.email "${GIT_USER_EMAIL}"
+
+apt update && apt -y install shellcheck && apt -y autopurge
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
 
 rm -rf public resources .hugo_build.lock
