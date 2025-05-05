d95a5001f1400a50b735097031bbf25362c6a0b3
zen96k <jolly96k@gmail.com>
2025-05-06 07:03:15 +0900

    chore: 開発環境を調整

--- .devcontainer/script/post_start_command.sh
+++ .devcontainer/script/post_start_command.sh
@@ -7,7 +7,12 @@ git config --global init.defaultBranch "${GIT_INIT_DEFAULT_BRANCH}"
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 
-apt update && apt -y install shellcheck && apt -y autopurge
+apt update && apt -y install zsh
+sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended
+chsh -s "$(which zsh)"
+cp -rfv .zshrc "${HOME}"
+
+apt update && apt -y install shellcheck
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
 
 rm -rf public resources .hugo_build.lock
